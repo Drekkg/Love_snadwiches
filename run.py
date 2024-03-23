c1af0659c9e8d36d9672236bb6e45f6de9973c65
drekkg <rizzagum@gmail.com>
2024-03-23 10:59:40 +0100

    FEAT adds loop until data is valid

--- run.py
+++ run.py
@@ -13,18 +13,28 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
 
-def get_sales():
-    """
-    Get sales input data from the user
-    """
-    print("Please enter the sales data from the last market")
-    print("Data should be six numbers, seperated by a comma")
-    print("Example: 10, 20, 33, 46, 60, 70")
+def get_sales_data():
+    while True:
+        """
+        Get sales input data from the user
+        """
+        print("Please enter the sales data from the last market.")
+        print("Data should be six numbers, seperated by a comma.")
+        print("Example: 10, 20, 33, 46, 60, 70")
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+        
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid.")
+            break
+
+    return sales_data   
+
+
+
+
 
 def validate_data(values):
     """
@@ -34,13 +44,16 @@ def validate_data(values):
     it cant.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values are required, you've entered {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.")
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
+    return True
 
-
-get_sales()
+data = get_sales_data()
+print(data)
