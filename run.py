21e42c35ca6d86b3bb56494100695bdc2dc10c7a
drekkg <rizzagum@gmail.com>
2024-03-23 09:52:06 +0100

    FEAT adds func to get sales data, and func to validate length

--- run.py
+++ run.py
@@ -12,8 +12,35 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-sales = SHEET.worksheet("sales")
 
-data = sales.get_all_values()
+def get_sales():
+    """
+    Get sales input data from the user
+    """
+    print("Please enter the sales data from the last market")
+    print("Data should be six numbers, seperated by a comma")
+    print("Example: 10, 20, 33, 46, 60, 70")
 
-print(data)
+    data_str = input("Enter your data here: ")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    checks to see if the inputted data is in the correct format,
+    six comma seperated numbers, 
+    Inside the try it will convert strings to ints and raise an error if 
+    it cant.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values are required, you've entered {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.")
+
+
+
+get_sales()
