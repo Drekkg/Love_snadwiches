9f0a7f4b0aabe96b1fcca98fa405fb0c098c093a
drekkg <rizzagum@gmail.com>
2024-03-28 07:24:43 +0100

    FEAT adds calculate stock func and adds to worksheet

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
 
 def get_sales_data():
-    # while True:
+    while True:
         """
         Get sales input data from the user
         """
@@ -23,16 +23,16 @@ def get_sales_data():
         print("Data should be six numbers, seperated by a comma.")
         print("Example: 10, 20, 33, 46, 60, 70")
 
-        # data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
         
-        # sales_data = data_str.split(",")
+        sales_data = data_str.split(",")
 
-        # if validate_data(sales_data):
-        #     print("Data is valid.")
-        #     break
-        sales_data = [12, 33, 55, 32, 12, 18]  
+        if validate_data(sales_data):
+            print("Data is valid.")
+            break
+        
 
-        return sales_data   
+    return sales_data   
 
 
 
@@ -107,6 +107,22 @@ def get_last_5_entries_sales():
         columns.append(column[-5:])
     return columns
 
+def calculate_stock_data(data):
+    """
+    calculate the average for each sandwich
+    """
+    print("Calculating stock data \n")
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+        
+    return new_stock_data
+        
+        
+
 
 def main():
     """
@@ -117,10 +133,12 @@ def main():
     update_worksheet(sales_data, "sales" )
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_column = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_column)
+    update_worksheet(stock_data, "stock")
 
 
 
 
 print("Welcome to Love Sandwiches data processing software")
-# main()
-sales_column = get_last_5_entries_sales()
+main()
